56cec040ee17c48c99ad27fee6bb7a2e49663faa
Kyle Johnson <kyleljohnson02@gmail.com>
2023-02-11 20:53:32 -0600

    Problem 1

---
+++ Cprogram1.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <math.h>
+
+#define months 12
+
+void readSalesData(double sales[], int numMonths);
+void printMonthlySalesReport(double sales[], int numMonths);
+void printMinMaxAvgSales(double sales[], int numMonths);
+void printSixMonthMovingAverages(double sales[], int numMonths);
+
+int main(void) {
+  double sales[months];
+
+  readSalesData(sales, months);
+  printMonthlySalesReport(sales, months);
+  printMinMaxAvgSales(sales, months);
+  printSixMonthMovingAverages(sales, months);
+
+  return 0;
+}
+
+void readSalesData(double sales[], int numMonths) {
+  int month;
+  FILE *inputFile;
+
+  // Open the file for reading
+  inputFile = fopen("sales_data.txt", "r");
+  if (inputFile == NULL) {
+    printf("Error opening file.\n");
+    return;
+  }
+
+  // Read sales data from file
+  for (month = 0; month < numMonths; ++month) {
+    fscanf(inputFile, "%lf", &sales[month]);
+  }
+
+  // Close the file
+  fclose(inputFile);
+}
+
+void printMinMaxAvgSales(double sales[], int numMonths) {
+  int i;
+  double sum, average, min, max;
+
+  // Find minimum, maximum, and average sales
+  min = max = average = sales[0];
+  for (i = 0; i < numMonths; ++i) {
+    sum += sales[i];
+    if (sales[i] < min) {
+      min = sales[i];
+    }
+    if (sales[i] > max) {
+      max = sales[i];
+    }
+  }
+  average = sum / numMonths;
+
+  // Print minimum, maximum, and average sales
+  printf("\nMinimum, Maximum and Average of the Monthly Salaries");
+  printf("\nMin: %.2lf", min);
+  printf("\nMax: %.2lf", max);
+  printf("\nAverage: %.2lf\n", average);
+}
+
+void printSixMonthMovingAverages(double sales[], int numMonths) {
+  int month, i;
+  double moving_average;
+
+  // Print six-month moving averages
+  printf("\nSix-month moving averages:\n");
+  for (month = 0; month < numMonths - 6; ++month) {
+    moving_average = 0.0;
+    for (i = 0; i < 6; ++i) {
+      moving_average += sales[month + i];
+    }
+    moving_average /= 6;
+    printf("%d: %.2lf\n", month + 1, moving_average);
+  }
+}
+
+void printMonthlySalesReport(double sales[], int numMonths) {
+  int i, j;
+
+  // Print monthly sales report from highest to lowest
+  printf("\nMonthly Sales Report (highest to lowest):\n");
+  for (i = 0; i < numMonths - 1; ++i) {
+    for (j = i + 1; j < numMonths; ++j) {
+      if (sales[i] < sales[j]) {
+        double temp = sales[i];
+        sales[i] = sales[j];
+        sales[j] = temp;
+        }
+      }
+    }
+  for (i = 0; i < numMonths; ++i) {
+    printf("%d: %.2lf\n", i + 1, sales[i]);
+  }
+  }
